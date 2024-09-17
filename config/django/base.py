4b12331cc6321ef7de714c9fa8bebe4b5c7e64a3
shahriar-fattahi <alifattahisf@gmail.com>
2024-09-17 21:55:46 +0330

    build: initial users app

--- config/django/base.py
+++ config/django/base.py
@@ -45,7 +45,9 @@ THIRD_PARTY_APPS = [
     "rest_framework",
     "drf_spectacular",
 ]
-LOCAL_APPS = []
+LOCAL_APPS = [
+    "apps.users.apps.UsersConfig",
+]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
