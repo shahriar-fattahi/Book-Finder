426c2ac739db0e53ded85949ef99c7406aa81372
shahriar-fattahi <alifattahisf@gmail.com>
2024-09-18 18:09:08 +0330

    feat: add book filter in Book Manager

--- apps/book/managers.py
+++ apps/book/managers.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional, Union
+from typing import Dict, List, Optional, Union
 
 from django.db import connection
 from pydantic import BaseModel
@@ -37,9 +37,9 @@ class BookManager(BaseModelManager):
                 raise e
             else:
                 data = dictfetchone(cursor=cursor)
-                book = Book(**data)
                 if data is None:
                     return None
+                book = Book(**data)
                 with connection.cursor() as cursor:
                     query = f"""
                         SELECT reviews.id, reviews.book_id, users_user.id AS user_id, users_user.username, rating 
@@ -65,6 +65,56 @@ class BookManager(BaseModelManager):
 
     def all(self, *args, **kwargs):
         return super().all(*args, **kwargs)
+
+    def filter(self, genre, user_id) -> List[BaseModel]:
+        from .schemas import Book, Review
+
+        with connection.cursor() as cursor:
+            try:
+                query = f"""
+                        SELECT id, title, author, genre FROM {self.table} 
+                        WHERE genre = %s;
+                    """
+                cursor.execute(
+                    query,
+                    [genre],
+                )
+            except Exception as e:
+                raise e
+            else:
+                data = dictfetchall(cursor=cursor)
+                if len(data) == 0:
+                    return []
+                ids = [e["id"] for e in data]
+
+                with connection.cursor() as cursor:
+                    query = f"""
+                        SELECT reviews.id, reviews.book_id, users_user.id AS user_id, users_user.username, rating 
+                        FROM reviews
+                        INNER JOIN users_user ON (reviews.user_id = users_user.id)
+                        WHERE reviews.book_id IN {*ids,} AND user_id = %s;
+                    """
+                    cursor.execute(query, [user_id])
+                    reviews = dictfetchall(cursor=cursor)
+
+                    books: List[Book] = []
+                    for d in data:
+                        book = Book(**d)
+                        books.append(book)
+                        for review in reviews:
+                            if book.id == review["book_id"]:
+                                user = UserSchema(
+                                    id=review["user_id"], username=review["username"]
+                                )
+                                book.reviews.append(
+                                    Review(
+                                        id=review["id"],
+                                        book=review["book_id"],
+                                        user=user,
+                                        rating=review["rating"],
+                                    )
+                                )
+                return books
 
     def update(self, *args, **kwargs):
         return super().update(*args, **kwargs)
